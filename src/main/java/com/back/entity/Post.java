a3ce9cbac134231e7360e4b4c87a6c435a8881e5
chan99k <kjkj5868@gmail.com>
2025-12-23 12:39:16 +0900

    게시글 댓글 엔티티 및 서비스 추가, 초기 데이터로 댓글 작성 기능 구현

--- src/main/java/com/back/entity/Post.java
+++ src/main/java/com/back/entity/Post.java
@@ -1,12 +1,16 @@
 package com.back.entity;
 
+import static jakarta.persistence.CascadeType.*;
 import static jakarta.persistence.FetchType.*;
+
+import java.util.List;
 
 import com.back.jpa.entity.BaseIdAndTime;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.NoArgsConstructor;
 
 @Entity
@@ -20,9 +24,25 @@ public class Post extends BaseIdAndTime {
 	@Column(columnDefinition = "LONGTEXT")
 	private String content;
 
+	@OneToMany(mappedBy = "post", cascade = {PERSIST, REMOVE}, orphanRemoval = true)
+	private List<PostComment> comments;
+
 	public Post(Member author, String title, String content) {
 		this.author = author;
 		this.title = title;
 		this.content = content;
 	}
+
+	public boolean hasComments() {
+		return !comments.isEmpty();
+	}
+
+	public PostComment addComment(Member author, String content) {
+		PostComment postComment = new PostComment(this, author, content);
+
+		comments.add(postComment);
+
+		return postComment;
+	}
+
 }
---
+++ src/main/java/com/back/entity/PostComment.java
@@ -0,0 +1,31 @@
+package com.back.entity;
+
+import static jakarta.persistence.FetchType.*;
+
+import com.back.jpa.entity.BaseIdAndTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.ManyToOne;
+import lombok.RequiredArgsConstructor;
+
+@Entity
+@RequiredArgsConstructor
+public class PostComment extends BaseIdAndTime {
+
+	@ManyToOne(fetch = LAZY)
+	private Post post;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	private Member member;
+
+	@Column(columnDefinition = "TEXT")
+	private String content;
+
+	public PostComment(Post parent, Member author, String content) {
+		this.content = content;
+		this.member = author;
+		this.post = parent;
+	}
+}
