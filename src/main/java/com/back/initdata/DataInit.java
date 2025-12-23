eb03710ebd6feacdb68acc9a32e0e399e491ba5b
chan99k <kjkj5868@gmail.com>
2025-12-23 12:06:51 +0900

    게시글 엔티티 및 서비스 추가, 초기 데이터로 게시글 6개 작성

--- src/main/java/com/back/initdata/DataInit.java
+++ src/main/java/com/back/initdata/DataInit.java
@@ -6,7 +6,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.back.entity.Member;
 import com.back.service.MemberService;
+import com.back.service.PostService;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -15,22 +17,27 @@ import lombok.extern.slf4j.Slf4j;
 public class DataInit {
 	private final DataInit self; // 자기 자신을 프록시로 호출하여 트랜잭션을 적용하기 위해
 	private final MemberService memberService;
+	private final PostService postService;
 
-	public DataInit(@Lazy DataInit self, MemberService memberService) { // 실제 프록시를 생성자 호출 시점이 아니라 나중에 초기화 -> 자기자신의 순환 참조 막기 위해
+	public DataInit(@Lazy DataInit self, MemberService memberService,
+		PostService postService) { // 실제 프록시를 생성자 호출 시점이 아니라 나중에 초기화 -> 자기자신의 순환 참조 막기 위해
 		this.self = self;
 		this.memberService = memberService;
+		this.postService = postService;
 	}
 
 	@Bean
 	public ApplicationRunner baseInitDataRunner() {
 		return args -> {
 			self.makeBaseMembers();
+			self.makeBasePosts();
 		};
 	}
 
 	@Transactional
 	public void makeBaseMembers() {
-		if (memberService.count() > 0) return;
+		if (memberService.count() > 0)
+			return;
 
 		memberService.join("system", "1234", "시스템");
 		memberService.join("holding", "1234", "홀딩");
@@ -38,5 +45,22 @@ public class DataInit {
 		memberService.join("user1", "1234", "유저1");
 		memberService.join("user2", "1234", "유저2");
 		memberService.join("user3", "1234", "유저3");
+	}
+
+	@Transactional
+	public void makeBasePosts() {
+		if (postService.count() > 0)
+			return;
+
+		Member user1Member = memberService.findByUsername("user1").orElseThrow();
+		Member user2Member = memberService.findByUsername("user2").orElseThrow();
+		Member user3Member = memberService.findByUsername("user3").orElseThrow();
+
+		postService.write(user1Member, "제목1", "내용1");
+		postService.write(user1Member, "제목2", "내용2");
+		postService.write(user1Member, "제목3", "내용3");
+		postService.write(user2Member, "제목4", "내용4");
+		postService.write(user2Member, "제목5", "내용5");
+		postService.write(user3Member, "제목6", "내용6");
 	}
 }
