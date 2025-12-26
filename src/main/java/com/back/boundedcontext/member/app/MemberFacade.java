8a3dbb07f8c98470b4b82449a38a95c3adae2b3a
chan99k <kjkj5868@gmail.com>
2025-12-26 09:30:58 +0900

    RsData 도입 - Member 모듈 내 도입

--- src/main/java/com/back/boundedcontext/member/app/MemberFacade.java
+++ src/main/java/com/back/boundedcontext/member/app/MemberFacade.java
@@ -3,10 +3,11 @@ package com.back.boundedcontext.member.app;
 import java.util.Optional;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.back.boundedcontext.member.domain.Member;
 import com.back.boundedcontext.member.out.MemberRepository;
-import com.back.global.exception.DomainException;
+import com.back.global.rsdata.RsData;
 
 import lombok.RequiredArgsConstructor;
 
@@ -14,23 +15,24 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class MemberFacade {
 	private final MemberRepository memberRepository;
+	private final MemberJoinUseCase memberJoinUseCase;
 
+	@Transactional(readOnly = true)
 	public long count() {
 		return memberRepository.count();
 	}
 
-	public void join(String username, String password, String nickname) {
-		findByUsername(username).ifPresent(m -> {
-			throw new DomainException("409-1", "이미 존재하는 username 입니다.");
-		});
-
-		memberRepository.save(new Member(username, password, nickname));
+	@Transactional
+	public RsData<Member> join(String username, String password, String nickname) {
+		return memberJoinUseCase.join(username, password, nickname);
 	}
 
+	@Transactional(readOnly = true)
 	public Optional<Member> findByUsername(String username) {
 		return memberRepository.findByUsername(username);
 	}
 
+	@Transactional(readOnly = true)
 	public Optional<Member> findById(int id) {
 		return memberRepository.findById(id);
 	}
--- src/main/java/com/back/boundedcontext/member/app/MemberJoinUseCase.java
+++ src/main/java/com/back/boundedcontext/member/app/MemberJoinUseCase.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import com.back.boundedcontext.member.domain.Member;
 import com.back.boundedcontext.member.out.MemberRepository;
 import com.back.global.exception.DomainException;
+import com.back.global.rsdata.RsData;
 
 import lombok.RequiredArgsConstructor;
 
@@ -13,11 +14,13 @@ import lombok.RequiredArgsConstructor;
 public class MemberJoinUseCase {
 	private final MemberRepository memberRepository;
 
-	public Member join(String username, String password, String nickname) {
+	public RsData<Member> join(String username, String password, String nickname) {
 		memberRepository.findByUsername(username).ifPresent(m -> {
 			throw new DomainException("409-1", "이미 존재하는 username 입니다.");
 		});
 
-		return memberRepository.save(new Member(username, password, nickname));
+		Member saved = memberRepository.save(new Member(username, password, nickname));
+
+		return new RsData<>("201-1", "%d번 회원이 생성되었습니다".formatted(saved.getId()), saved);
 	}
 }
